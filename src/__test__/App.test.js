5109afb40cd8c26c946a92ff4b19402aa92891b8
amitha-priyangi <81142542+amitha-priyangi@users.noreply.github.com>
2023-06-14 11:35:48 +0530

    modify app and login test

---
+++ src/__test__/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import store from '../app/store'
+import { Provider } from 'react-redux';
+
+test('renders Logout page', () => {
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
+    const name = screen.getByPlaceholderText(/Name/i);
+    expect(name).toBeInTheDocument();
+});
--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,8 +1,38 @@
-// import { getByRole, render, screen } from '@testing-library/react';
-// import Login from '../Component/Login';
+import { render, screen } from '@testing-library/react';
+import Login from '../Component/Login';
+import store from '../app/store'
+import { Provider } from 'react-redux';
 
-// test("renders Logout page", () => {
-//     render(
-//         <Login />);
-//     expect(screen.getByText(/Login Here/i)).toBeInTheDocument();
-// });
+describe('Login', () => {
+    test('renders Logout page', () => {
+        render(
+            <Provider store={store}>
+                <Login />
+            </Provider>
+        );
+        const name = screen.getByPlaceholderText(/Name/i);
+        expect(name).toBeInTheDocument();
+    });
+
+    test('render the login page with 1 button', async () => {
+        render(
+            <Provider store={store}>
+                <Login />
+            </Provider>
+        );
+        const buttonCount = await screen.findAllByRole('button');
+        expect(buttonCount).toHaveLength(1);
+    });
+
+    // test('render the login page with 3 input', async () => {
+    //     render(
+    //         <Provider store={store}>
+    //             <Login />
+    //         </Provider>
+    //     );
+    //     const inputCount = await screen.findAllByRole('button');
+    //     expect(inputCount).toHaveLength(3);
+    // });
+
+});
+
